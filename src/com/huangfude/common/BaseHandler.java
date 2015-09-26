7f3dcdbfa19f0bf2d8030dbdf8a8adc61552605a
manville <imanville@foxmail.com>
2015-09-26 23:57:04 +0900

    fix login

--- src/com/huangfude/common/BaseHandler.java
+++ src/com/huangfude/common/BaseHandler.java
@@ -12,7 +12,7 @@ public class BaseHandler extends Handler {
 	public void handle(String target, HttpServletRequest request,HttpServletResponse response, boolean[] isHandled) {
 
 		//如果路径中包含admin(该路径下所有资源),那么验证身份,通过验证才能进入后台管理模块
-		if(target.indexOf("admin")>0 && target.indexOf("admin/login")<0){
+		if(target.indexOf("admin")>0 && target.indexOf("admin/login")<0 && target.indexOf("admin/css")<0 ){
 			HttpSession session = request.getSession(false);
 			if(session==null||session.getAttribute("user")==null){
 				//验证不成功，跳转到后台登陆页面
